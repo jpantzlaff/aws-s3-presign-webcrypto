7eab6ae0c03ae492de12da244c77b848baf24480
John Pantzlaff <john.pantzlaff@bayer.com>
2023-12-07 16:05:14 -0700

    Include built files in repo

---
+++ dist/mod-browser.js
@@ -0,0 +1,123 @@
+/// <reference types="./mod.d.ts" />
+const NEWLINE = "\n";
+export function encodeString(data) {
+  return new TextEncoder().encode(data);
+}
+function hex(data) {
+  return Array.from(new Uint8Array(data)).map((x) => x.toString(16).padStart(2, "0")).join("");
+}
+export async function sha256(data) {
+  const digest = await crypto.subtle.digest("SHA-256", encodeString(data));
+  return hex(digest);
+}
+async function hmacSha256(keyData, data) {
+  const algorithm = {
+    name: "HMAC",
+    hash: "SHA-256"
+  };
+  const key = await crypto.subtle.importKey(
+    "raw",
+    keyData,
+    algorithm,
+    false,
+    ["sign"]
+  );
+  return crypto.subtle.sign(
+    algorithm,
+    key,
+    encodeString(data)
+  );
+}
+export async function hmacSha256Hex(key, data) {
+  const signature = await hmacSha256(key, data);
+  return hex(signature);
+}
+function ymd(date) {
+  return date.toISOString().substring(0, 10).replace(/[^\d]/g, "");
+}
+function isoDate(date) {
+  return `${date.toISOString().substring(0, 19).replace(/[^\dT]/g, "")}Z`;
+}
+function parseOptions(provided) {
+  const path = `/${provided.path}`.replace(/\/\//g, "/");
+  return {
+    ...{
+      method: "GET",
+      region: "us-east-1",
+      expiresIn: 86400,
+      date: /* @__PURE__ */ new Date(),
+      sessionToken: "",
+      endpoint: "s3.amazonaws.com",
+      query: {},
+      protocol: "https"
+    },
+    ...provided,
+    path
+  };
+}
+function getQueryParameters(options) {
+  return new URLSearchParams({
+    "X-Amz-Algorithm": "AWS4-HMAC-SHA256",
+    "X-Amz-Credential": `${options.accessKeyId}/${ymd(options.date)}/${options.region}/s3/aws4_request`,
+    "X-Amz-Date": isoDate(options.date),
+    "X-Amz-Expires": options.expiresIn.toString(),
+    "X-Amz-SignedHeaders": "host",
+    ...options.sessionToken ? { "X-Amz-Security-Token": options.sessionToken } : {},
+    ...options.query
+  });
+}
+function getCanonicalRequest(options, queryParameters) {
+  queryParameters.sort();
+  return [
+    options.method,
+    NEWLINE,
+    options.path,
+    NEWLINE,
+    queryParameters.toString(),
+    NEWLINE,
+    `host:${options.endpoint}`,
+    NEWLINE,
+    NEWLINE,
+    "host",
+    NEWLINE,
+    "UNSIGNED-PAYLOAD"
+  ].join("");
+}
+async function getSignaturePayload(options, payload) {
+  return [
+    "AWS4-HMAC-SHA256",
+    NEWLINE,
+    isoDate(options.date),
+    NEWLINE,
+    `${ymd(options.date)}/${options.region}/s3/aws4_request`,
+    NEWLINE,
+    await sha256(payload)
+  ].join("");
+}
+async function getSignatureKey(options) {
+  let key = encodeString(`AWS4${options.secretAccessKey}`);
+  const components = [
+    ymd(options.date),
+    options.region,
+    "s3",
+    "aws4_request"
+  ];
+  for (const component of components) {
+    key = await hmacSha256(key, component);
+  }
+  return key;
+}
+function getUrl(options, queryParameters, signature) {
+  queryParameters.set("X-Amz-Signature", signature);
+  return `${options.protocol}://${options.endpoint}${options.path}?${new URLSearchParams(queryParameters).toString()}`;
+}
+export async function getSignedUrl(options) {
+  const parsedOptions = parseOptions(options);
+  const queryParameters = getQueryParameters(parsedOptions);
+  const canonicalRequest = getCanonicalRequest(parsedOptions, queryParameters);
+  const signaturePayload = await getSignaturePayload(parsedOptions, canonicalRequest);
+  const signatureKey = await getSignatureKey(parsedOptions);
+  const signature = await hmacSha256Hex(signatureKey, signaturePayload);
+  const url = getUrl(parsedOptions, queryParameters, signature);
+  return url;
+}
